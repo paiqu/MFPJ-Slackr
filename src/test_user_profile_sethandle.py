883710e401a1280db88df97173ac06fc0c0633f7
Haofu CHEN <z5237481@student.unsw.edu.au>
2020-03-03 23:31:54 +1100

    Fixed some Errors

--- src/test_user_profile_sethandle.py
+++ src/test_user_profile_sethandle.py
@@ -8,18 +8,21 @@ InputError:
 The first case:handle string entered between 3 and 20 character
 The second case: handle string entered is already used
 """
-
+#This function is to test the first case
 def test_user_handle_inputError_one():
 #create the user
     user_one = auth_register("timm123@icloud.com", "TimmLiew", "Timm", "Li")
     user_id_one = user_one["u_id"]
     token_one = user_one["token"]
 #if the user entered out of 20 character->InputError
-    with pytest.raise(InputError)
-        user_profile_setname(token_one,'a' * 1000)
+    with pytest.raises(InputError):
+        user_profile_sethandle(token_one,'a' * 1000)
 #if the user entered between 0 and 2->InputError
-    with pytest.raise(InputError)
+    with pytest.raises(InputError):
         user_profile_sethandle(token_one,'b' * 2)    
+
+
+#This function is to test teh second case
 def test_user_handle_inputError_two():
 #create the user
     user_two = auth_register("timm3@icloud.com", "TiLiw", "Taammaaaaaaaa", "Aaaaaaaa")
@@ -30,7 +33,7 @@ def test_user_handle_inputError_two():
     user_id_three = user_three["u_id"]
     token_three = user_three["token"]
 #if handle string is already existed->InputError
-    with pytest.raise(InputError)
-        user_profile_sethandle(token_three,Taammaaaaaaaa Aaaaaaaa)
+    with pytest.raises(InputError):
+        user_profile_sethandle(token_three,'Taammaaaaaaaa Aaaaaaaa')
     
     
