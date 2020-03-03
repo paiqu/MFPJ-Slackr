7768f1c371bdada474ee50272361c536cdd3a1d5
Xinlei Zhang <z5237609@ad.unsw.edu.au>
2020-03-04 03:19:50 +1100

    uncompleted version 1.0

---
+++ src/user_test_user_profile.py
@@ -0,0 +1,25 @@
+import pytest
+from auth import auth_register
+from user import user_profile_sethandle
+'''
+This is a test file for user_profile function
+
+InputError: User with u_id is not a valid user
+
+1. test for normal case 
+    (For a valid user, returns information about 
+    their email, first name, last name, and handle)
+    
+2. test for InputError
+'''
+
+def test_user_profile():
+    # register for a new user
+    newUser = auth_register('z5237609@unsw.edu.au', 'Zxl471238986', 'Matty', 'Zhang')
+    newUser_id = newUser['u_id']
+    newUser_token = newUser['token']
+    
+    # give a handle name to user
+    user_profile_sethandle(newUser_token, 'MattyLei')
+    
+    user_profile(newUser_token,newUser_id)
