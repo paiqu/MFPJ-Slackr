ebd63b37b266e9936c7c62c4b71d768e6b90f6ee
Xinlei Zhang <z5237609@ad.unsw.edu.au>
2020-03-04 03:47:05 +1100

    Completed version 1.0

--- src/user_test_user_profile.py
+++ src/user_test_user_profile.py
@@ -1,6 +1,8 @@
 import pytest
 from auth import auth_register
 from user import user_profile_sethandle, user_profile
+from error import InputError
+
 '''
 This is a test file for user_profile function
 
@@ -16,23 +18,35 @@ InputError: User with u_id is not a valid user
 def test_user_profile():
     
     # register for a new user
-    newUser = auth_register('z5237609@unsw.edu.au', 'Zxl471238986', 'Matty', 'Zhang')
-    newUser_id = newUser['u_id']
-    newUser_token = newUser['token']
+    newUser1 = auth_register('z5237609@unsw.edu.au', 'Zxl471238986', 'Matty', 'Zhang')
+    newUser1_id = newUser1['u_id']
+    newUser1_token = newUser1['token']
     
     # give a handle name to user
-    user_profile_sethandle(newUser_token, 'MattyLei')
+    user_profile_sethandle(newUser1_token, 'MattyLei')
     
-    returnUser = user_profile(newUser_token, newUser_id)
+    returnUser1 = user_profile(newUser1_token, newUser1_id)
     
     # get my return value
-    returnUser_email = returnUser['email']
-    returnUser_first = returnUser['name_first']
-    returnUser_last = returnUser['name_last']  
-    returnUser_handle = returnUser['handle_str']
+    returnUser1_email = returnUser1['email']
+    returnUser1_first = returnUser1['name_first']
+    returnUser1_last = returnUser1['name_last']  
+    returnUser1_handle = returnUser1['handle_str']
     
     # check whether they are same
-    assert returnUser_email == 'z5237609@unsw.edu.au'
-    assert returnUser_first == 'Matty'
-    assert returnUser_last == 'Zhang'
-    assert returnUser_handle == 'MattyLei'
+    assert returnUser1_email == 'z5237609@unsw.edu.au'
+    assert returnUser1_first == 'Matty'
+    assert returnUser1_last == 'Zhang'
+    assert returnUser1_handle == 'MattyLei'
+    
+def test_user_profile_inputError():
+
+    # register for a new user
+    newUser2 = auth_register('z1234567@unsw.edu.au', 'dfdsbuS123', 'Haofu', 'Chen')
+    newUser2_id = newUser2['u_id']
+    newUser2_token = newUser2['token']
+          
+    with pytest.raises(InputError):
+        user_profile(newUser2_token, newUser2_id + 4)
+    
+
