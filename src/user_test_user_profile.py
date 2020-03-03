79257f94962076cfdbc331bdfb540ecb859eea6f
Xinlei Zhang <z5237609@ad.unsw.edu.au>
2020-03-04 03:34:37 +1100

    Uncompleted version 1.1 - Slightly change

--- src/user_test_user_profile.py
+++ src/user_test_user_profile.py
@@ -1,6 +1,6 @@
 import pytest
 from auth import auth_register
-from user import user_profile_sethandle
+from user import user_profile_sethandle, user_profile
 '''
 This is a test file for user_profile function
 
@@ -14,6 +14,7 @@ InputError: User with u_id is not a valid user
 '''
 
 def test_user_profile():
+    
     # register for a new user
     newUser = auth_register('z5237609@unsw.edu.au', 'Zxl471238986', 'Matty', 'Zhang')
     newUser_id = newUser['u_id']
@@ -22,4 +23,16 @@ def test_user_profile():
     # give a handle name to user
     user_profile_sethandle(newUser_token, 'MattyLei')
     
-    user_profile(newUser_token,newUser_id)
+    returnUser = user_profile(newUser_token, newUser_id)
+    
+    # get my return value
+    returnUser_email = returnUser['email']
+    returnUser_first = returnUser['name_first']
+    returnUser_last = returnUser['name_last']  
+    returnUser_handle = returnUser['handle_str']
+    
+    # check whether they are same
+    assert returnUser_email == 'z5237609@unsw.edu.au'
+    assert returnUser_first == 'Matty'
+    assert returnUser_last == 'Zhang'
+    assert returnUser_handle == 'MattyLei'
