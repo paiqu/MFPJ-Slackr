ebf9178ef4db338472735254913b73bc6b8b2eaa
Haofu CHEN <z5237481@student.unsw.edu.au>
2020-03-04 11:51:50 +1100

    Fixed case 2

--- src/test_user_profile_sethandle.py
+++ src/test_user_profile_sethandle.py
@@ -25,8 +25,9 @@ def test_user_handle_inputError_one():
 #This function is to test teh second case
 def test_user_handle_inputError_two():
 #create the user
-    user_two = auth_register("timm3@icloud.com", "TiLiw", "Taammaaaaaaaa", "Aaaaaaaa")
+    user_two = auth_register("timm3@icloud.com", "TiLiw", "Taammaaaaa", "Aaaaaa")
     user_id_two = user_two["u_id"]
+    user_two_handle = user_two["handle"]
     token_two = user_two["token"]
 #create the another user
     user_three = auth_register("timasdm123@icloud.com", "TimdsmLiew", "Dandmm", "Lmi")
@@ -34,6 +35,6 @@ def test_user_handle_inputError_two():
     token_three = user_three["token"]
 #if handle string is already existed->InputError
     with pytest.raises(InputError):
-        user_profile_sethandle(token_three,'Taammaaaaaaaa Aaaaaaaa')
+        user_profile_sethandle(token_three,'handle')
     
     
