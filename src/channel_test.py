e43c844d8a2c79543fb7f8ac8ca040a516acbaf0
Juliette Ingis <z5209488@unsw.edu.au>
2020-03-04 22:06:26 +1100

    Created Another Test for Channel_Invites: Checking whether the user had already been invited to the group

--- src/channel_test.py
+++ src/channel_test.py
@@ -2,7 +2,7 @@ from channel import channel_invite, channel_details, channel_messages
 from auth import auth_login, auth_register, auth_logout 
 from channels import channels_create
 import pytest
-from error import InputError
+from error import InputError, AccessError
 
 def test_channel_invite_correct():
     user1 = auth_register('student.test@unsw.edu.au','!@678hello', 'Student', 'Test')
@@ -28,13 +28,29 @@ def test_channel_exist():
 
 #Tests if valid user (if user exists)
 
-def test_channel_valid_user():
+def test_channel_invalid_user():
     channel1 = channels_create('token', 'Channel1', True)
     c_id1=channe1['u_id']
     
     with pytest.raises(InputError) as e:
         channel_invite('token',c_id1, 'u_id2')
-
+        
+        
+#user added twice        
+def test_channel_user_twice():
+    user1 = auth_register('student.test@unsw.edu.au','!@678hello', 'Student', 'Test')
+    u_id1=user1['u_id']
+    
+    channel1 = channels_create('token', 'Channel1', True)
+    c_id1=channe1['u_id']
+    
+    channel_invite('token', c_id1,u_id1)
+    
+    with pytest.raises(AccessError) as e:
+        channel_invite('token', c_id1,u_id1)
+        
+        
+    
         
     
     
