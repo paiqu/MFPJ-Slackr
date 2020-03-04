75418278adb9406e726a1b5cc55617a72d677018
Juliette Ingis <z5209488@unsw.edu.au>
2020-03-04 22:21:14 +1100

    Added Function_Correct Tests for the channel details test

--- src/channel_test.py
+++ src/channel_test.py
@@ -9,18 +9,18 @@ def test_channel_invite_correct():
     u_id1=user1['u_id']
     
     channel1 = channels_create('token', 'Channel1', True)
-    c_id1=channe1['u_id']
+    c_id1=channe1['channel_id']
     
     channel_invite('token', c_id1,u_id1)
 
 
 #Tests if channel Exists
-def test_channel_exist():
+def test_channel_exists_invite():
     user1 = auth_register('student.test@unsw.edu.au','!@678hello', 'Student', 'Test')
     u_id1=user1['u_id']
     
     channel1 = channels_create('token', 'Channel1', True)
-    c_id1=channe1['u_id']
+    c_id1=channe1['channel_id']
     
     with pytest.raises(InputError) as e:
         channel_invite('token','c_id2', 'u_id1')
@@ -28,27 +28,49 @@ def test_channel_exist():
 
 #Tests if valid user (if user exists)
 
-def test_channel_invalid_user():
+def test_channel_invite_invalid_user():
     channel1 = channels_create('token', 'Channel1', True)
-    c_id1=channe1['u_id']
+    c_id1=channe1['channel_id']
     
     with pytest.raises(InputError) as e:
         channel_invite('token',c_id1, 'u_id2')
         
         
 #user added twice        
-def test_channel_user_twice():
+def test_channel_invite_user_twice():
     user1 = auth_register('student.test@unsw.edu.au','!@678hello', 'Student', 'Test')
     u_id1=user1['u_id']
     
     channel1 = channels_create('token', 'Channel1', True)
-    c_id1=channe1['u_id']
+    c_id1=channe1['channel_id']
     
     channel_invite('token', c_id1,u_id1)
     
     with pytest.raises(AccessError) as e:
         channel_invite('token', c_id1,u_id1)
         
+## THE FOLLOWING FUNCTIONS ARE TESTS FOR CHANNEL_DETAILS
+
+def test_channel_details_correct():
+    user1 = auth_register('student.test@unsw.edu.au','!@678hello', 'Student', 'Test')
+    u_id1=user1['u_id']
+    
+    channel1 = channels_create('token', 'Channel1', True)
+    c_id1=channe1['channel_id']
+    
+    channel_invite('token', c_id1,u_id1)
+    
+    user1 = auth_register('student.test@unsw.edu.au','!@678hello', 'Student', 'Test')
+    u_id1=user1['u_id']
+    
+    channel1 = channels_create('token', 'Channel1', True)
+    c_id1=channe1['u_id']
+    
+    assert channel_details('token', c_id1) == {
+        'name' : 'Channel1',
+        'owner_members': [u_id1], 
+        'all_members': [u_id1],
+    }    
         
     
         
