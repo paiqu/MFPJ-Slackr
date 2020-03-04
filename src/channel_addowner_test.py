a0fb075b180a984623c27e413b2649edba8fef65
Pai Qu <pai.qu@outlook.com>
2020-03-04 15:26:50 +1100

    inputError test function finished

--- src/channel_addowner_test.py
+++ src/channel_addowner_test.py
@@ -1,3 +1,9 @@
+import pytest
+from channel import channel_addowner, channel_details
+from channels import channels_create
+from error import InputError, AccessError
+from auth import auth_register
+
 '''
 channel_addowner(token, channel_id, u_id) -> {}
 
@@ -13,20 +19,13 @@ AccessError:
 
 Tests:
     1. succuess: user2 become the owner of user1's channel
-    2. InputError_1
-    3. InputError_2
-    4. AccessError_1
+    2. InputError_1: invalid channel_id
+    3. InputError_2: add A as its own channel's owner
+    4. AccessError_1: 
     5. AccessError_2
 
 ASSUME: there could be multiple owners for one channel
 '''
-import pytest
-from channel import channel_addowner, channel_details
-from channels import channels_create
-from error import InputError, AccessError
-from auth import auth_register
-
-
 
 def test_channel_addowner_success():
     # Register for user1
@@ -60,4 +59,33 @@ def test_channel_addowner_success():
 
     assert is_in == True
 
-  
+def test_channel_addowner_inputError_1():
+    # Register for user1
+    user1 = auth_register('z123456@unsw.edu.au', 'thisisaPassword', 'First', 'Last')
+    u_id1 = user1['u_id']
+    token1 = user1['token']
+
+    # Register for user2
+    user2 = auth_register('z654321@unsw.edu.au', 'thisisaGoodPassword', 'New', 'Guy')
+    u_id2 = user2['u_id']
+    token2 = user2['token']
+
+    # user1 creates a public channel
+    channel_id = (channels_create(token1, 'channel1', True))['channel_id']
+
+    with pytest.raises(InputError):
+        # invalid channel_id
+        channel_addowner(token2, channel_id + 6, u_id2)
+
+def test_channel_addowner_inputError_2():
+    # Register for user1
+    user1 = auth_register('z123456@unsw.edu.au', 'thisisaPassword', 'First', 'Last')
+    u_id1 = user1['u_id']
+    token1 = user1['token']
+
+    # user1 creates a public channel
+    channel_id = (channels_create(token1, 'channel1', True))['channel_id']
+
+    with pytest.raises(InputError):
+        channel_addowner(token1, channel_id, u_id1)
+
