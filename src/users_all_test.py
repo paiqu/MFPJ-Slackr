7c3a6dae2b37b64255c128b65bc9de3c6cefeb35
Haofu CHEN <z5237481@student.unsw.edu.au>
2020-03-05 11:34:09 +1100

    Fixed some problems

--- src/users_all_test.py
+++ src/users_all_test.py
@@ -1,27 +1,40 @@
 import pytest
-from user import user_profile, users_all
+from user import user_profile,user_profile_sethandle
 from auth import auth_register
+from other import users_all
 """
 Test return the correct list of users and informations
 """
 #This function is to test the normal case to return correct informations
 def test_users_all_success():
-#create a new user 
-    user_one = auth_register('z123456@unsw.edu.au', 'z123456', 'Honey', 'Pii')
+    #create a new user1 
+    user_one = auth_register('z123456@unsw.edu.au', 'z123456', 'Honey', 'Jii')
     user_one_id = user_one["u_id"]
-    token_one = user_one["token"]
-    
-    return_user_one = user_profile(token_one, user_one_id)
-
-    # store the return value
-    return_one_email = return_user_one["email"]
-    return_one_first_name = return_user_one["name_first"]
-    return_one_last_name = return_user_one["name_last"]  
-
+    token_one = user_one["token"] 
+    user_one_handle = user_profile_sethandle(token_one, 'hfdjshg')
+ 
+     #create a new user2 
+    user_two = auth_register('z5237534@unsw.edu.au', 'z1234534', 'Candy', 'JiJi')
+    user_two_id = user_two["u_id"]
+    token_two = user_two["token"] 
+    user_two_handle = user_profile_sethandle(token_two, 'Chocolate')
     # check whether they are same
-    assert return_one_email == 'z123456@unsw.edu.au'
-    assert return_one_first_name == 'Honey'
-    assert return_one_last_name == 'Pii'
+    usersDict = users_all(token_one)['users'] # list of dict
+    
+    assert len(usersDict) == 2
+    assert usersDict[0]['u_id'] == user_one_id
+    assert usersDict[0]['email'] == 'z123456@unsw.edu.au'
+    assert usersDict[0]['name_first'] == 'Honey'
+    assert usersDict[0]['name_last'] == 'Jii'
+    assert usersDict[0]['handle_str'] == 'hfdjshg'
+    
+    assert usersDict[1]['u_id'] == user_one_id
+    assert usersDict[1]['email'] == 'z123456@unsw.edu.au'
+    assert usersDict[1]['name_first'] == 'Candy'
+    assert usersDict[1]['name_last'] == 'JiJi'
+    assert usersDict[1]['handle_str'] == 'Chocolate'
+
+
 
 
 
