28ed910ce51042ccb130aaa92d03ca698cd2d887
Juliette Ingis <z5209488@unsw.edu.au>
2020-03-06 11:23:41 +1100

    Added a separate file for login tests

--- src/auth_login_test.py
+++ src/auth_login_test.py
@@ -4,54 +4,57 @@ from error import InputError
 
 #Tests for the function auth_register 
 def test_register_correct():
-    user_one = register('student.test@unsw.edu.au','!@678hello', 'Student', 'Test')
+    user_one = auth_register('student.test@unsw.edu.au','!@678hello', 'Student', 'Test')
     
 def test_register_short_password():
+    '''
+    Testing too short password, should raise an error
+    '''
     with pytest.raises(InputError) as e:
-        register('student.test@unsw.edu.au','678', 'Student', 'Test')
+        auth_register('student.test@unsw.edu.au','678', 'Student', 'Test')
         
 def test_register_invalid_email():
     with pytest.raises(InputError) as e:
-        register('student.testunsw.edu.au','!@678hello', 'Student', 'Test')
+        auth_register('student.testunsw.edu.au','!@678hello', 'Student', 'Test')
         
 def test_register_invalid_fname():
     with pytest.raises(InputError) as e:
-        register('student.test@unsw.edu.au','!@678hello', '', 'Test')
+        auth_register('student.test@unsw.edu.au','!@678hello', '', 'Test')
         
 def test_register_invalid_lname():
     with pytest.raises(InputError) as e:
-        register('student.test@unsw.edu.au','!@678hello', 'Student', '')
+        auth_register('student.test@unsw.edu.au','!@678hello', 'Student', '')
         
 def test_register_twice():
-    user_one = register('student.test@unsw.edu.au','!@678hello', 'Student', 'Test')
+    user_one = auth_register('student.test@unsw.edu.au','!@678hello', 'Student', 'Test')
 
     with pytest.raises(InputError) as e:
-        user_two = register('student.test@unsw.edu.au','!@678hello', 'Student', 'Test')
+        user_two = auth_register('student.test@unsw.edu.au','!@678hello', 'Student', 'Test')
     
 #Tests for the function auth_login     
 #check that the user has registered an account 
 def test_login_correct():
-    user_one = register('student.test@unsw.edu.au','!@678hello', 'Student', 'Test')
-    login_user = login('student.test@unsw.edu.au','!@678hello')
+    user_one = auth_register('student.test@unsw.edu.au','!@678hello', 'Student', 'Test')
+    login_user = auth_login('student.test@unsw.edu.au','!@678hello')
 
 def test_login_user_exists():
-    user_one = register('student.test@unsw.edu.au','!@678hello', 'Student', 'Test') 
+    user_one = auth_register('student.test@unsw.edu.au','!@678hello', 'Student', 'Test') 
     u_id1 = user_one['u_id']
     token1 = user_one['token']
     
     
-    login_user = login('student.test@unsw.edu.au','!@678hello')
+    login_user = auth_login('student.test@unsw.edu.au','!@678hello')
     u_id2 = login_user['u_id']
     token2= login_user['token']
     
     assert u_id1 == u_id2
     
 def test_login_incorrect_password():
-    user_one = register('student.test@unsw.edu.au','!@678hello', 'Student', 'Test')
+    user_one = auth_register('student.test@unsw.edu.au','!@678hello', 'Student', 'Test')
     
-    login_user = login('student.test@unsw.edu.au','!@678hello')
+    login_user = auth_login('student.test@unsw.edu.au','hello')
     
-    assert user_one['password'] == login_user['password']
+    assert user_one['password'] != login_user['password']
     
     
 def test_login_no_email_domain():
@@ -61,38 +64,7 @@ def test_login_no_email_domain():
         
         
 def test_login_no_email_atsign():
-    user_one = register('student.test@unsw.edu.au','!@678hello', 'Student', 'Test')
+    user_one = auth_register('student.test@unsw.edu.au','!@678hello', 'Student', 'Test')
     
     with pytest.raises(InputError) as e:
-        login_user = login('student.testunsw.edu.au','!@678hello', 'Student', 'Test')
-    
-def test_logout_correct():
-    user_one = register('student.test@unsw.edu.au','!@678hello', 'Student', 'Test')
-    login_user = login('student.test@unsw.edu.au','!@678hello')
-    u_id1 = login_user['u_id']
-    token1 = login_user['token']
-    
-    logout(login_user['token'])
-    assert login_user['token'] == True
-
-def test_logout_twice():
-    user_one = register('student.test@unsw.edu.au','!@678hello', 'Student', 'Test')
-    login_user = login('student.test@unsw.edu.au','!@678hello')
-    u_id1 = login_user['u_id']
-    token1 = login_user['token']
-    
-    logout(login_user['token'])
-    logout(login_user['token'])
-    assert login_user['token'] == False
-    
-    
-"""
-def test_echo():
-    assert echo.echo("1") == "1", "1 == 1"
-    assert echo.echo("abc") == "abc", "abc == abc"
-    assert echo.echo("trump") == "trump", "trump == trump"
-
-def test_echo_except():
-    with pytest.raises(InputError) as e:
-        assert echo.echo("echo")
-"""
+        login_user = auth_login('student.testunsw.edu.au','!@678hello', 'Student', 'Test')
