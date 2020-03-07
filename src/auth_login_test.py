61cdaa8db4a6a2372cec7531dfbec861edac6d35
Juliette Ingis <z5209488@unsw.edu.au>
2020-03-07 19:49:33 +1100

    Added more test functions to the login function

--- src/auth_login_test.py
+++ src/auth_login_test.py
@@ -12,9 +12,33 @@ def test_login_correct():
     user_one = auth_register('student.test@unsw.edu.au','!@678hello', 'Student', 'Test')
     login_user = auth_login('student.test@unsw.edu.au','!@678hello')
 
+
+def test_login_no_email_domain():
+    """
+    Testing when user attempts to log in without entering their email domain.
+    Email entered is not a valid email using the method provided in Project Specs.
+    """     
+    user_one = auth_register('student.test@unsw.edu.au','!@678hello', 'Student', 'Test')
+
+    with pytest.raises(InputError) as e:
+        login_user = auth_login('student.test@','!@678hello')    
+        
+        
+def test_login_no_email_atsign():
+    """
+    Testing when user attempts to log in without entering @ within their email.
+    Email entered is not a valid email using the method provided in Project Specs.
+    """
+    user_one = auth_register('student.test@unsw.edu.au','!@678hello', 'Student', 'Test')
+    
+    with pytest.raises(InputError) as e:
+        login_user = auth_login('student.testunsw.edu.au','!@678hello')
+
+
 def test_login_user_exists():
     """
-    Testing when user attempts to log in but their account does not exist
+    Testing when user attempts to log in but their account does not exist.
+    Email entered does not belong to a user.
     """
     ### FIX THIS
     user_one = auth_register('student.test@unsw.edu.au','!@678hello', 'Student', 'Test') 
@@ -22,12 +46,15 @@ def test_login_user_exists():
     token1 = user_one['token']
     
     
+    with pytest.raises(InputError) as e:
+        auth_login('user_doesnt_exist@unsw.edu.au','I_am_incorrect')
+    '''
+    assert u_id1 != u_id2
     login_user = auth_login('student.test@unsw.edu.au','!@678hello')
     u_id2 = login_user['u_id']
     token2= login_user['token']
-    
-    assert u_id1 == u_id2
-    
+    '''
+
 def test_login_incorrect_password():
     """
     Testing when user attempts to log in with the incorrect password
@@ -38,22 +65,3 @@ def test_login_incorrect_password():
         auth_login('student.test@unsw.edu.au','I_am_incorrect')
     
     
-def test_login_no_email_domain():
-    """
-    Testing when user attempts to log in without entering their email domain
-    """     
-    user_one = auth_register('student.test@unsw.edu.au','!@678hello', 'Student', 'Test')
-
-    with pytest.raises(InputError) as e:
-        login_user = auth_login('student.test@','!@678hello')
-        
-        
-        
-def test_login_no_email_atsign():
-    """
-    Testing when user attempts to log in without entering @ within their email 
-    """
-    user_one = auth_register('student.test@unsw.edu.au','!@678hello', 'Student', 'Test')
-    
-    with pytest.raises(InputError) as e:
-        login_user = auth_login('student.testunsw.edu.au','!@678hello')
