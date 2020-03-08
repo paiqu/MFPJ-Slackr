c929a8ef571c9abf2e139d2dd78aa4a5a0060f6c
Haofu CHEN <z5237481@student.unsw.edu.au>
2020-03-09 09:45:40 +1100

    Fixing the format

--- src/users_all_test.py
+++ src/users_all_test.py
@@ -8,29 +8,33 @@ Test return the correct list of users and informations
     #This function is to test the normal case to return correct informations
 def test_users_all_success():
     
-    #create a new user1 
+    #register the first user 
     user_one = auth_register('z123456@unsw.edu.au', 'z123456', 'Honey', 'Jii')
-    user_one_id = user_one["u_id"]
-    token_one = user_one["token"] 
+    user_one_id = user_one['u_id']
+    token_one = user_one['token'] 
     user_profile_sethandle(token_one, 'hfdjshg')
  
-    #create a new user2 
+    #register the second user 
     user_two = auth_register('z5237534@unsw.edu.au', 'z1234534', 'Candy', 'JiJi')
-    user_two_id = user_two["u_id"]
-    token_two = user_two["token"] 
+    user_two_id = user_two['u_id']
+    token_two = user_two['token'] 
     user_profile_sethandle(token_two, 'Chocolate')
     
     # check whether they are same
     usersDict = users_all(token_one)['users']
     
+    #check the length of dic
     assert len(usersDict) == 2
+    
+    #check the first user information are matched
     assert usersDict[0]['u_id'] == user_one_id
     assert usersDict[0]['email'] == 'z123456@unsw.edu.au'
     assert usersDict[0]['name_first'] == 'Honey'
     assert usersDict[0]['name_last'] == 'Jii'
     assert usersDict[0]['handle_str'] == 'hfdjshg'
     
-    assert usersDict[1]['u_id'] == user_one_id
+    #check  the second user information are matched
+    assert usersDict[1]['u_id'] == user_two_id
     assert usersDict[1]['email'] == 'z123456@unsw.edu.au'
     assert usersDict[1]['name_first'] == 'Candy'
     assert usersDict[1]['name_last'] == 'JiJi'
