28802a192e1d5a6dccf94d4b151270078c794a42
Xinlei Zhang <z5237609@ad.unsw.edu.au>
2020-03-09 00:02:05 +1100

    Completed Version 4.0 change variable name

--- src/test_message_send.py
+++ src/test_message_send.py
@@ -36,15 +36,15 @@ def test_message_send_case1():
     newUser_token = newUser['token']
     
     # create a channel
-    newChannel_2 = channels_create(newUser_token, 'General', True)
-    channel_ID_2 = newChannel_2['channel_id']
+    newChannel = channels_create(newUser_token, 'General', True)
+    channel_ID = newChannel['channel_id']
 
     # send message
-    sending = message_send(newUser_token, channel_ID_2, 'Nice to meet u')
+    sending = message_send(newUser_token, channel_ID, 'Nice to meet u')
     message_id = sending['message_id']
 
     # list channel messages
-    messageReturn = channel_messages(newUser_token, channel_ID_2, 0)
+    messageReturn = channel_messages(newUser_token, channel_ID, 0)
     
     # checking
     assert messageReturn['messages'][0]['message_id'] == message_id
@@ -59,36 +59,36 @@ def test_message_send_case2():
     '''
     
     # register for new users
-    newUser4 = auth_register('z47123898@unsw.edu.au', 'safjiefijew322', 'Lei', 'Zhang')
-    newUser4_id = newUser4['u_id']
-    newUser4_token = newUser4['token']
+    newUser1 = auth_register('z47123898@unsw.edu.au', 'safjiefijew322', 'Lei', 'Zhang')
+    newUser1_id = newUser1['u_id']
+    newUser1_token = newUser1['token']
 
-    newUser5 = auth_register('z5237609@unsw.edu.au', 'Zxl471238986', 'Matty', 'Zhang')
-    newUser5_id = newUser5['u_id']
-    newUser5_token = newUser5['token']
+    newUser2 = auth_register('z5237609@unsw.edu.au', 'Zxl471238986', 'Matty', 'Zhang')
+    newUser2_id = newUser2['u_id']
+    newUser2_token = newUser2['token']
         
     # create a channel
-    newChannel_3 = channels_create(newUser4_token, 'General', False)
+    newChannel_3 = channels_create(newUser1_token, 'General', False)
     channel_ID_3 = newChannel_3['channel_id']
     
     # invite user
-    channel_invite(newUser4_token, channel_ID_3, newUser5_id)
+    channel_invite(newUser1_token, channel_ID_3, newUser2_id)
 
     # send message
-    sending1 = message_send(newUser4_token, channel_ID_3, 'Nice to meet u.')
+    sending1 = message_send(newUser1_token, channel_ID_3, 'Nice to meet u.')
     message_id1 = sending1['message_id']
 
-    sending2 = message_send(newUser5_token, channel_ID_3, 'Nice to meet u too.')
+    sending2 = message_send(newUser2_token, channel_ID_3, 'Nice to meet u too.')
     message_id2 = sending2['message_id']
     
-    sending3 = message_send(newUser4_token, channel_ID_3, 'How are you?')
+    sending3 = message_send(newUser1_token, channel_ID_3, 'How are you?')
     message_id3 = sending3['message_id']
 
-    sending4 = message_send(newUser5_token, channel_ID_3, 'I am good.')
+    sending4 = message_send(newUser2_token, channel_ID_3, 'I am good.')
     message_id4 = sending4['message_id']
         
     # list channel messages
-    messageReturn1 = channel_messages(newUser4_token, channel_ID_3, 0)
+    messageReturn1 = channel_messages(newUser1_token, channel_ID_3, 0)
     
     # checking
     assert messageReturn1['messages'][0]['message_id'] == message_id4
@@ -152,24 +152,24 @@ def test_message_send_accessError_case2():
     '''
     
     # register for new users
-    newUser6 = auth_register('z1234567@unsw.edu.au', 'dfhsiH4723', 'First', 'Last')
-    newUser6_id = newUser6['u_id']
-    newUser6_token = newUser6['token']
+    newUser4 = auth_register('z1234567@unsw.edu.au', 'dfhsiH4723', 'First', 'Last')
+    newUser4_id = newUser4['u_id']
+    newUser4_token = newUser4['token']
 
-    newUser7 = auth_register('z7654321@unsw.edu.au', '4298dhihiHIh', 'Foster', 'Chen')
-    newUser7_id = newUser7['u_id']
-    newUser7_token = newUser7['token']
+    newUser5 = auth_register('z7654321@unsw.edu.au', '4298dhihiHIh', 'Foster', 'Chen')
+    newUser5_id = newUser5['u_id']
+    newUser5_token = newUser5['token']
         
     # create a channel
-    newChannel_4 = channels_create(newUser6_token, 'Random', True)
+    newChannel_4 = channels_create(newUser4_token, 'Random', True)
     channel_ID_4 = newChannel_4['channel_id']
     
     # join channel
-    channel_join(newUser7_token, channel_ID_4)
+    channel_join(newUser5_token, channel_ID_4)
     
     # leave this channel
-    channel_leave(newUser7_token, channel_ID_4)
+    channel_leave(newUser5_token, channel_ID_4)
     
     # send message with an unauthorised user (accesserror)
     with pytest.raises(AccessError):
-        message_send(newUser7_token, channel_ID_4, 'Hello World')  
+        message_send(newUser5_token, channel_ID_4, 'Hello World')  
