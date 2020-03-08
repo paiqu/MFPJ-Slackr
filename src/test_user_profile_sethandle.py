4a79a3cfc3712846593a39f54822c6d44505ced3
Haofu CHEN <z5237481@student.unsw.edu.au>
2020-03-08 11:44:13 +1100

    Adding the success one

--- src/test_user_profile_sethandle.py
+++ src/test_user_profile_sethandle.py
@@ -4,36 +4,55 @@ from auth import auth_register
 from error import InputError
 
 """
+Test the normal case:
+
 InputError:
 The first case:handle string entered between 3 and 20 character
 The second case: handle string entered is already used
 """
-#This function is to test the first case
+    #Thsi function is to tets the normal case
+def test_user_handle_success():
+    #create the user
+    user_zero = auth_register("z4357621@unde.edu.au", "T64782", "Tim", "Puilet")
+    user_id_zero = user_zero["u_id"]
+    token_zero = user_zero["token"]
+    
+    #check whether it will equal the handle str
+    user_zero_handle = user_profile_sethandle[token_zero,'Hhhh']
+    assert(user_zero_handle == 'Hhhh')
+    
+    #This function is to test the first case
 def test_user_handle_inputError_one():
-#create the user
+    
+    #create the user
     user_one = auth_register("timm123@icloud.com", "TimmLiew", "Timm", "Li")
     user_id_one = user_one["u_id"]
     token_one = user_one["token"]
-#if the user entered out of 20 character->InputError
+    
+    #if the user entered out of 20 character->InputError
     with pytest.raises(InputError):
         user_profile_sethandle(token_one,'a' * 1000)
-#if the user entered between 0 and 2->InputError
+    
+    #if the user entered between 0 and 2->InputError
     with pytest.raises(InputError):
         user_profile_sethandle(token_one,'b' * 2)    
 
 
-#This function is to test teh second case
+    #This function is to test teh second case
 def test_user_handle_inputError_two():
-#create the user
+    
+    #create the user
     user_two = auth_register("timm3@icloud.com", "TiLiw", "Taammaaaaa", "Aaaaaa")
     user_id_two = user_two["u_id"]
     user_two_handle = user_two["handle"]
     token_two = user_two["token"]
-#create the another user
+    
+    #create the another user
     user_three = auth_register("timasdm123@icloud.com", "TimdsmLiew", "Dandmm", "Lmi")
     user_id_three = user_three["u_id"]
     token_three = user_three["token"]
-#if handle string is already existed->InputError
+    
+    #if handle string is already existed->InputError
     with pytest.raises(InputError):
         user_profile_sethandle(token_three,'handle')
     
