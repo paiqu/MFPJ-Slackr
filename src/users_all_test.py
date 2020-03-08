77b11de8b4006fd42d2f74dd4e5f4c5015032e5c
Haofu CHEN <z5237481@student.unsw.edu.au>
2020-03-08 23:10:40 +1100

    Fixing the format

--- src/users_all_test.py
+++ src/users_all_test.py
@@ -5,21 +5,23 @@ from other import users_all
 """
 Test return the correct list of users and informations
 """
-#This function is to test the normal case to return correct informations
+    #This function is to test the normal case to return correct informations
 def test_users_all_success():
+    
     #create a new user1 
     user_one = auth_register('z123456@unsw.edu.au', 'z123456', 'Honey', 'Jii')
     user_one_id = user_one["u_id"]
     token_one = user_one["token"] 
-    user_one_handle = user_profile_sethandle(token_one, 'hfdjshg')
+    user_profile_sethandle(token_one, 'hfdjshg')
  
-     #create a new user2 
+    #create a new user2 
     user_two = auth_register('z5237534@unsw.edu.au', 'z1234534', 'Candy', 'JiJi')
     user_two_id = user_two["u_id"]
     token_two = user_two["token"] 
-    user_two_handle = user_profile_sethandle(token_two, 'Chocolate')
+    user_profile_sethandle(token_two, 'Chocolate')
+    
     # check whether they are same
-    usersDict = users_all(token_one)['users'] # list of dict
+    usersDict = users_all(token_one)['users']
     
     assert len(usersDict) == 2
     assert usersDict[0]['u_id'] == user_one_id
