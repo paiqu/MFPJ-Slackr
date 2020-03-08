d2527c7200eabf66bcde948c6b20d9b5cdce6267
Haofu CHEN <z5237481@student.unsw.edu.au>
2020-03-08 22:49:33 +1100

    Adding more success case and fixed some problems about the format

--- src/test_user_profile_sethandle.py
+++ src/test_user_profile_sethandle.py
@@ -4,56 +4,100 @@ from auth import auth_register
 from error import InputError
 
 """
-Test the normal case:
+Test the normal case: 
+One user set the handle_str
+Two user set the handle_str
 
 InputError:
 The first case:handle string entered between 3 and 20 character
 The second case: handle string entered is already used
 """
-    #Thsi function is to tets the normal case
-def test_user_handle_success():
+    #This function will test the normal case that one user set the handle name 
+def test_user_handle_success_one():
+    
     #create the user
-    user_zero = auth_register("z4357621@unde.edu.au", "T64782", "Tim", "Puilet")
-    user_id_zero = user_zero["u_id"]
-    token_zero = user_zero["token"]
+    user_zero = auth_register('z4357621@unde.edu.au', 'T64782', 'Tim', 'Puilet')
+    user_id_zero = user_zero['u_id']
+    token_zero = user_zero['token']
     
-    #check whether it will equal the handle str
-    user_zero_handle = user_profile_sethandle[token_zero,'Hhhh']
-    assert(user_zero_handle == 'Hhhh')
+    #set the new handle string
+    user_zero_handle = user_profile_sethandle(token_zero,'Hemil')
     
-    #This function is to test the first case
+    #check whether the handle can be setted
+    user_dict = user_profile(token_zero, 'u_id')
+    
+    assert user_dict['user']['handle_str'] == 'Hem'
+    
+    #This function will test the success case that two user set the handle name
+def test_user_handle_success_two():
+    #create the user
+    user_one = auth_register('z9327481@unsw.edu.au', 'Geily7481', 'Grey', 'Eily')
+    user_id_one = user_one['u_id']
+    token_one = user_one['token']
+    
+    #create the another user
+    user_two = auth_register('z8466789@unsw.edu.au', 'TimdsmLiew', 'Dnne', 'Lmi')
+    user_id_two = user_two["u_id"]
+    token_two = user_two["token"]
+    
+    #set the new handle string for first user
+    user_one_handle = user_profile_sethandle(token_one,'Grhy')
+    
+    #set the new handle string for second user
+    user_two_handle = user_profile_sethandle(token_two,'Dlle')
+    
+    #check whether the handle can be setted for first user
+    user_dict = user_profile(token_one, 'u_id')
+    
+    #check whether the handle can be setted for second user
+    user_dict_two = user_profile(token_two, 'u_id')
+    
+    assert user_dict['user']['handle_str'] == 'Grhy'
+    assert user_dict['user']['handle_str'] == 'Dlle'
+       
+    #This function is to test when the handle out of 20 character
 def test_user_handle_inputError_one():
     
     #create the user
-    user_one = auth_register("timm123@icloud.com", "TimmLiew", "Timm", "Li")
-    user_id_one = user_one["u_id"]
-    token_one = user_one["token"]
+    user_one = auth_register('z2137682@unsw.edu.au', 'TimmLiew', 'Timm', 'Li')
+    user_id_one = user_one['u_id']
+    token_one = user_one['token']
     
     #if the user entered out of 20 character->InputError
     with pytest.raises(InputError):
         user_profile_sethandle(token_one,'a' * 1000)
     
-    #if the user entered between 0 and 2->InputError
-    with pytest.raises(InputError):
-        user_profile_sethandle(token_one,'b' * 2)    
-
-
-    #This function is to test teh second case
+    #This function will test when the handle between 0 and 2
 def test_user_handle_inputError_two():
     
     #create the user
-    user_two = auth_register("timm3@icloud.com", "TiLiw", "Taammaaaaa", "Aaaaaa")
-    user_id_two = user_two["u_id"]
-    user_two_handle = user_two["handle"]
-    token_two = user_two["token"]
+    user_one = auth_register('z6925391@unsw.edu.au', 'Fly5391', 'Tinne', 'Lily')
+    user_id_one = user_one['u_id']
+    token_one = user_one['token']
+        
+    #if the user entered between 0 and 2->InputError
+    with pytest.raises(InputError):
+        user_profile_sethandle(token_one,'a' * 2)    
+
+
+    #This function is to test teh second case
+def test_user_handle_inputError_three():
+    
+    #create the user
+    user_two = auth_register('z6677442@unsw.edu.au', 'TiLiw', 'Tmmaeno', 'Aaabc')
+    user_id_two = user_two['u_id']
+    token_two = user_two['token']
     
     #create the another user
-    user_three = auth_register("timasdm123@icloud.com", "TimdsmLiew", "Dandmm", "Lmi")
-    user_id_three = user_three["u_id"]
-    token_three = user_three["token"]
+    user_three = auth_register('z5327481@unsw.edu.au', 'TimdsmLiew', 'Dandmm', 'Lmi')
+    user_id_three = user_three['u_id']
+    token_three = user_three['token']
+    
+    #set the handle for the first user
+    user_two_handle = user_profile_sethandle(token_two,'TimAaab')
     
     #if handle string is already existed->InputError
     with pytest.raises(InputError):
-        user_profile_sethandle(token_three,'handle')
+        user_profile_sethandle(token_three,'TimAaab')
     
     
