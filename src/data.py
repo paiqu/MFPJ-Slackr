058dbd310ff7d7da0352c9d235befbea9813220c
Xinlei Zhang <xinlei.zhang1@student.unsw.edu.au>
2020-03-26 09:15:52 +1100

    Update data.py add new data store

--- src/data.py
+++ src/data.py
@@ -1,10 +1,11 @@
-from class_file import User
+from class_file import User, Channel, Message, Time, React
 
 DATA = {
     'users': [], # a list of users {u_id: user_1}
     'channels': [], # a list of channels  {channel_id: channel_1}
-    'messages': [] # a list of messages
-    'times': [] # a list of time
+    'messages': [], # a list of messages
+    'times': [], # a list of time
+    'reacts':[] # a list of reacts
 }
 
 SECRET = 'MFPJ'
