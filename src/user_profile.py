70629d751953f622332882d063cc9d84671082e1
Xinlei Zhang <xinlei.zhang1@student.unsw.edu.au>
2020-03-26 14:58:11 +1100

    Update user_profile.py finished route check

--- src/user_profile.py
+++ src/user_profile.py
@@ -1,9 +1,7 @@
 '''Route implementation for user/profile'''
 from json import dumps
 from flask import Blueprint, request
-from check_functions import token_to_uid
 from error import InputError
-from class_file import User
 from data import DATA, getData
 
 PROFILE = Blueprint('profile', __name__)
@@ -14,37 +12,34 @@ def request_get():
     '''function for route user/profile'''
     token = request.args.get('token')
     u_id = int(request.args.get('u_id'))
-    return dumps(user_profile(token, u_id))
+    return user_profile(token, u_id)
 
 def user_profile(token, u_id):
     '''
     For a valid user, returns information about their user id, email, 
     first name, last name, and handle
-
-    Input Error:
-        invlid channel_id
-    
-    ASSUME: the token id is valid
     '''
-    user_id = token_to_uid(token)
-    if user_id != u_id:
-        raise InputError("Invalid u_id")
-
+    
     DATA = getData()
+    is_exist = False
     
+    # check Input Error (User with u_id is not a valid user)
+    for user in DATA['users']
+        if user['u_id'] = u_id
+            is_exist = True
+    if not is_exist:
+        raise InputError("Invalid u_id")
+    
+    # get user profile
     user = {'user':{}}
-    
     for existuser in DATA['users']:
-        if existuser.u_id == user_id:
-            user['user']['u_id'] = existuser.u_id
-            user['user']['email'] = existuser.email
-            user['user']['name_first'] = existuser.name_first
-            user['user']['name_last'] = existuser.name_last
-            user['user']['handle_str'] = existuser.handle
+        if existuser['u_id'] == u_id:
+            user['user']['u_id'] = existuser['u_id']
+            user['user']['email'] = existuser['email']
+            user['user']['name_first'] = existuser['name_first']
+            user['user']['name_last'] = existuser['name_last']
+            user['user']['handle_str'] = existuser['handle']
             
     return user
-
-
-
-       
-
+    
+    
