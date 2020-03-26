00f64f9aae07ef586f7afc24924d527087dc6d7f
zebin <z5237137@ad.unsw.edu.au>
2020-03-26 14:47:58 +1100

    1

--- src/channels_list.py
+++ src/channels_list.py
@@ -1,41 +1,33 @@
 # Route implementation for channels/route
 from json import dumps
 from flask import Blueprint, request
-from check_functions import channel_id_check, channel_member_check, token_to_uid
-from error import InputError, AccessError
-
+from check_functions import channel_member_check
 from class_file import Channel
-from data import *
+from data import DATA, getData
 
 CHANNELS_LIST = Blueprint('channels_list', __name__)
 
 @CHANNELS_LIST.route('/channels/list', methods=['GET'])
-def list():
+def route_channels_list():
     # function for route channels/list
-    token = request.form.get('token')
-    channel_id = request.form.get('channnel_id')
-    return dumps(channels_list(token, channel_id))
+    token = request.args.get('token')
+    return dumps(channels_list(token))
     
 def channels_list(token):
     '''
     Given a token, list all channels that user is a part of 
     
     '''
-    new_list = []
     
     global DATA
     DATA = getData()
+    channels = DATA['channels']
     
+    new_list = []
     for channel in DATA['channels']:
-        if channel_member_check(channel.channel_id, token):
-            new_dict = {}
-            new_dict['channel_id'] = channel.channel_id
-            new_dict['name'] = channel.name
-            new_list.append(new_dict)
-            
+        if channel_member_check(channel['channel_id'], token):
+            new_list.append(channel)
+    
+    return_dict = {}        
     return_dict['channels'] = new_list
     return return_dict
-            
-    
-    
-
--- src/channels_listall.py
+++ src/channels_listall.py
@@ -1,19 +1,14 @@
 # Route implementation for channels/route
 from json import dumps
 from flask import Blueprint, request
-from check_functions import channel_id_check, channel_member_check, token_to_uid
-from error import InputError, AccessError
-
-from class_file import Channel
-from data import *
+from data import DATA, getData
 
 LEAVE = Blueprint('channels_listall', __name__)
 
 @LEAVE.route('/channels/listall', methods=['POST'])
 def listall():
     # function for route channels/list
-    token = request.form.get('token')
-    channel_id = request.form.get('channnel_id')
+    token = request.args.get('token')
     return dumps(channels_list(token, channel_id))
     
 def channels_listall(token):
@@ -21,16 +16,7 @@ def channels_listall(token):
     Given a token, list all channels 
     
     '''
-    new_list = []
     
     global DATA
     DATA = getData()
-    
-    for user in DATA['users']: 
-        if user.u_id == token_to_uid(token):
-            return DATA['channels']
-        
-           
-    
-    
-
+    return DATA['channels']
