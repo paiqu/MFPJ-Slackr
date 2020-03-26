34b7cc2d1f429b48b97fc4098f39d13ab20753e8
Xinlei Zhang <z5237609@ad.unsw.edu.au>
2020-03-26 20:36:29 +1100

    unfinished

--- src/search.py
+++ src/search.py
@@ -3,6 +3,7 @@ from json import dumps
 from flask import Blueprint, request
 from check_functions import token_to_uid
 from data import DATA, getData
+from class_file import User, React, Message, Channel
 
 SEARCH = Blueprint('search', __name__)
 
@@ -11,9 +12,8 @@ SEARCH = Blueprint('search', __name__)
 def request_get():
     '''function for route user/profile'''
     token = request.args.get('token')
-    query_str = str(request.args.get('query_str'))
-    search(token,query_str)
-    return dumps({})
+    query_str = request.args.get('query_str')
+    return dumps(search(token,query_str))
 
 def search(token, query_str):
     '''
@@ -29,9 +29,15 @@ def search(token, query_str):
     reacts = DATA['reacts']
     
     messagelist = {'messages':[]}
-        
+    users.append(vars(User(u_id=123, email='123@55.com', name_first='pai', name_last='qu', handle='')))   
+    channels.append(vars(Channel(1,"Public")))
+    channels[0]['members'].append(users[0])
+    channels[0]['owners'].append(users[0])
+    messages.append(vars(Message("hello",1,1,123,574820)))
+    reacts.append(vars(React(1,123,1)))
+
     for message in messages:
-        if query_str in message['message_content']:
+        if str(query_str) in message['message_content']:
             message_info = {}
             message_info['message_id'] = message['message_id']
             message_info['u_id'] = message['sender_id']
@@ -40,26 +46,34 @@ def search(token, query_str):
             
             react_collect = []
             for react in reacts:
-                if react['message_id'] = message['message_id']:
-                    for react_single in react_collect:
-                        
-                        is_react_in == False
-                        if react_single['react_id'] == react['react_id']:
-                            react_collect['react_single']['u_ids'].append(react['u_id'])
-                            is_react_in == True
-                        
-                        if is_react_in == False
-                            react_collect.append({'react_id': react['react_id'], 'u_ids': [react['u_id']], 'is_this_user_reacted': False})
+                
+                if react['message_id'] == message['message_id']:
                     
-                    for i in react_collect:
-                        for user in i['u_ids']:
-                            if token_to_uid(token) = user:
-                                is_this_user_reacted = True
-             
-            message_info['reacts'] = react_collect          
+                    if react_collect == []:
+                        react_collect.append({'react_id': react['react_id'], 'u_ids': [react['u_id']], 'is_this_user_reacted': False})
+
+                    else:   
+                        for react_single in react_collect:
+                        
+                            is_react_in = False
+                            if react_single['react_id'] == react['react_id']:
+                                react_collect['react_single']['u_ids'].append(react['u_id'])
+                                is_react_in = True
+                        
+                            if is_react_in == False:
+                                react_collect.append({'react_id': react['react_id'], 'u_ids': [react['u_id']], 'is_this_user_reacted': False})
+            '''       
+            for i in react_collect:
+                for user in i['u_ids']:
+                    if token_to_uid(token) == user:
+                        i['is_this_user_reacted'] = True
+            '''       
+
+            message_info['reacts'] = react_collect 
+                     
             message_info['is_pinned'] = message['is_pin']
     
-            messagelist['messages'].append = message_info
+            messagelist['messages'].append(message_info)
             
     return messagelist
 
