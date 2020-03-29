630873a93ae2bcfe3aab6d8868bec41f88ab105b
Haofu Chen <haofu.chen@student.unsw.edu.au>
2020-03-29 20:48:21 +1100

    Update user_all.py

--- src/user_all.py
+++ src/user_all.py
@@ -6,17 +6,17 @@ from error import InputError, AccessError
 from class_file import User
 from data import *
 
-ALL = Blueprint('user_all', __name__)
+ALL = Blueprint('users_all', __name__)
 
-@ALL.route('/user/all', methods=['GET'])
+@ALL.route('/users/all', methods=['GET'])
 def all():
     '''function for route of users all'''
     token = request.args.get('token')
     u_id = request.args.get('u_id')
-    return dumps(user_all(token, u_id))
+    return dumps(users_all(token, u_id))
    
 
-def user_all(token, u_id):
+def users_all(token, u_id):
     '''
     Given a token, list all users associated with information 
     
@@ -24,9 +24,5 @@ def user_all(token, u_id):
 
     DATA = getData()
     users = DATA['users']
-
-    users.append(vars(User(u_id = 23, email = 'z54634652@unsw.edu.au', name_first = 'Matiea', name_last = 'Ben', handle = '')))
-    
-    user = {'user':{}}
       
     return users
