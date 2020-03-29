68e7400a7d91fe2247ad6c70bcdaf9756e61ae41
Zebin Chen <zebin.chen1@student.unsw.edu.au>
2020-03-29 14:09:32 +1100

    Update channels_listall.py

--- src/channels_listall.py
+++ src/channels_listall.py
@@ -22,14 +22,14 @@ def channels_listall(token):
     DATA = getData()
     users = DATA['users']
     channels = DATA['channels']
-    
+    '''
     users.append(vars(User(u_id=1, email='123@55.com', name_first='mike', name_last='cop', handle='')))
     channels.append(vars(Channel(channel_id = 1, channel_name = 'name')))
     channels[0]['members'].append(users[0])
     channels[0]['owners'].append(users[0])
     
     channels.append(vars(Channel(channel_id = 2, channel_name = 'NAME')))
-            
+    '''        
     return_dict = {}        
     return_dict['channels'] = channels
     return return_dict
