43adf4e58dd390e9dcf2bdaad93fa5253dbb3c58
Xinlei Zhang <z5237609@ad.unsw.edu.au>
2020-03-30 13:52:44 +1100

    slight change

--- src/valid_email.py
+++ src/valid_email.py
@@ -1,27 +1,27 @@
 '''
 This code has been sourced on the Geeks For Geeks Website 
-As Per the Specs of my COMP1531 Project 
+As Per the Specs of my COMP1531 Project
 '''
 
-# Python program to validate an Email 
+# Python program to validate an Email
+
+# import re module
+
+# re module provides support
+# for regular expressions
+import re
   
-# import re module 
-  
-# re module provides support 
-# for regular expressions 
-import re 
-  
-# Make a regular expression 
-# for validating an Email 
+# Make a regular expression
+# for validating an Email
 regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
       
-# Define a function for 
-# for validating an Email 
+# Define a function for
+# for validating an Email
 def check(email):  
   
-    # pass the regualar expression 
-    # and the string in search() method 
-    if(re.search(regex,email)):  
+    # pass the regualar expression
+    # and the string in search() method
+    if(re.search(regex,email)):
         print("Valid Email") 
         return True 
           
@@ -44,3 +44,4 @@ if __name__ == '__main__' :
   
     email = "ankitrai326.com"
     check(email) 
+
