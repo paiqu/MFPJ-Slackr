9e513372c33f0441db68ce7eb1df77b366d67496
Pai Qu <pai.qu@outlook.com>
2020-03-30 18:12:11 +1100

    finish channel_join_http_test

---
+++ src/system_tests/channel_join_httpy_test.py
@@ -0,0 +1,212 @@
+'''
+Steps to test channel/join:
+    1. Register two users: user_1 and user_2
+    2. Both users login to the server
+    3. the user_1 create a channel (public)
+    4. Time to Test:
+        1. user_2 joins user_1's public channel
+        2. Input Error:
+            invalid channel_id
+        3. Access Error:
+            join private channel
+'''
+
+
+import sys
+sys.path.append('..')
+from json import load, dumps
+import urllib.request
+import urllib.parse
+import pytest
+from data import DATA
+
+PORT_NUMBER = '5204'
+BASE_URL = 'http://127.0.0.1:' + PORT_NUMBER
+#BASE_URL now is 'http://127.0.0.1:5321'
+
+@pytest.fixture
+def register_and_login_user_1_and_2():
+    # RESET
+    req = urllib.request.Request(
+        f'{BASE_URL}/workspace/reset',
+        headers={'Content-Type': 'application/json'},
+        method='POST'
+    )
+
+    load(urllib.request.urlopen(req))
+
+    # REGISTER user_1
+    register_info_1 = dumps({
+        'email': 'z1234567@unsw.edu.au',
+        'password': 'thisisaPassword',
+        'name_first': 'Peter',
+        'name_last': 'Parker'
+    }).encode('utf-8')
+
+
+    req = urllib.request.Request(
+        f'{BASE_URL}/auth/register',
+        data=register_info_1,
+        headers={'Content-Type': 'application/json'},
+        method='POST'
+    )
+
+    load(urllib.request.urlopen(req))
+    
+    # REGISTER user_2
+    register_info_2 = dumps({
+        'email': 'z7654321@unsw.edu.au',
+        'password': 'thisisaPassword',
+        'name_first': 'Pete',
+        'name_last': 'Peteer'
+    }).encode('utf-8')
+
+
+    req = urllib.request.Request(
+        f'{BASE_URL}/auth/register',
+        data=register_info_2,
+        headers={'Content-Type': 'application/json'},
+        method='POST'
+    )
+
+    load(urllib.request.urlopen(req))
+    
+    # Login user_1
+    login_info = dumps({
+        'email': 'z1234567@unsw.edu.au',
+        'password': 'thisisaPassword'
+    }).encode('utf-8')
+
+    req = urllib.request.Request(
+        f'{BASE_URL}/auth/login',
+        data=login_info,
+        headers={'Content-Type': 'application/json'},
+        method='POST'
+    )
+
+    # Login user_2
+    login_info = dumps({
+        'email': 'z7654321@unsw.edu.au',
+        'password': 'thisisaPassword'
+    }).encode('utf-8')
+
+    req = urllib.request.Request(
+        f'{BASE_URL}/auth/login',
+        data=login_info,
+        headers={'Content-Type': 'application/json'},
+        method='POST'
+    )
+
+    load(urllib.request.urlopen(req))
+    #return payload
+
+def test_join_public(register_and_login_user_1_and_2):
+    user_1_token = 'b\'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1X2lkIjoiMSJ9.N0asY15U0QBAYTAzxGAvdkuWG6CyqzsR_rvNQtWBmLg\''
+    user_2_token = 'b\'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1X2lkIjoiMiJ9.UNGv0HfSeyM4FtXkAc4HfuOl_HyNLFmRMeLx_4c0Ryg\''
+    
+    # user_1 creates a public channel
+    channel_info = dumps({
+        'token': user_1_token,
+        'name': 'a channel',
+        'is_public': True
+    }).encode('utf-8')
+
+    req = urllib.request.Request(
+        f'{BASE_URL}/channels/create',
+        data=channel_info,
+        headers={'Content-Type': 'application/json'},
+        method='POST'
+    )
+
+    load(urllib.request.urlopen(req))
+
+    # user_2 join user_1's channel
+    join_info = dumps({
+        'token': user_2_token,
+        'channel_id': 1
+    }).encode('utf-8')
+
+    req = urllib.request.Request(
+        f'{BASE_URL}/channel/join',
+        data=join_info,
+        headers={'Content-Type': 'application/json'},
+        method='POST'
+    )
+
+    payload = load(urllib.request.urlopen(req))
+    assert payload == {}
+
+
+def test_for_invalid_channel_id(register_and_login_user_1_and_2):
+    user_1_token = 'b\'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1X2lkIjoiMSJ9.N0asY15U0QBAYTAzxGAvdkuWG6CyqzsR_rvNQtWBmLg\''
+    user_2_token = 'b\'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1X2lkIjoiMiJ9.UNGv0HfSeyM4FtXkAc4HfuOl_HyNLFmRMeLx_4c0Ryg\''
+    
+    # user_1 creates a public channel
+    channel_info = dumps({
+        'token': user_1_token,
+        'name': 'a channel',
+        'is_public': True
+    }).encode('utf-8')
+
+    req = urllib.request.Request(
+        f'{BASE_URL}/channels/create',
+        data=channel_info,
+        headers={'Content-Type': 'application/json'},
+        method='POST'
+    )
+
+    load(urllib.request.urlopen(req))
+
+    # user_2 join user_1's channel
+    join_info = dumps({
+        'token': user_2_token,
+        'channel_id': 50
+    }).encode('utf-8')
+
+    req = urllib.request.Request(
+        f'{BASE_URL}/channel/join',
+        data=join_info,
+        headers={'Content-Type': 'application/json'},
+        method='POST'
+    )
+
+    with pytest.raises(urllib.error.HTTPError):
+        urllib.request.urlopen(req)
+
+
+def test_join_private_channel(register_and_login_user_1_and_2):
+    user_1_token = 'b\'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1X2lkIjoiMSJ9.N0asY15U0QBAYTAzxGAvdkuWG6CyqzsR_rvNQtWBmLg\''
+    user_2_token = 'b\'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1X2lkIjoiMiJ9.UNGv0HfSeyM4FtXkAc4HfuOl_HyNLFmRMeLx_4c0Ryg\''
+    
+    # user_1 creates a public channel
+    channel_info = dumps({
+        'token': user_1_token,
+        'name': 'a channel',
+        'is_public': False
+    }).encode('utf-8')
+
+    req = urllib.request.Request(
+        f'{BASE_URL}/channels/create',
+        data=channel_info,
+        headers={'Content-Type': 'application/json'},
+        method='POST'
+    )
+
+    load(urllib.request.urlopen(req))
+
+    # user_2 join user_1's channel
+    join_info = dumps({
+        'token': user_2_token,
+        'channel_id': 1
+    }).encode('utf-8')
+
+    req = urllib.request.Request(
+        f'{BASE_URL}/channel/join',
+        data=join_info,
+        headers={'Content-Type': 'application/json'},
+        method='POST'
+    )
+
+
+    with pytest.raises(urllib.error.HTTPError):
+        urllib.request.urlopen(req)
