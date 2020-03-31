654ca102e8045e017ce1d08f27be8cda7cc38374
Pai Qu <pai.qu@outlook.com>
2020-03-31 14:47:53 +1100

    submit tests

---
+++ src/system_tests/workspace_reset_http_test.py
@@ -0,0 +1,213 @@
+'''
+Steps to reset a workspace:
+1. create and login two users
+2. they create some channels
+3. reset
+4. test:
+    channel/listall = {'channels':[]}
+    users/all = {'users':[]}
+'''
+import sys
+sys.path.append('..')
+from json import load, dumps
+import urllib.request
+import urllib.parse
+import pytest
+from data import DATA
+
+PORT_NUMBER = '5204'
+BASE_URL = 'http://127.0.0.1:' + PORT_NUMBER
+#BASE_URL now is 'http://127.0.0.1:5321'
+
+@pytest.fixture
+def register_and_login_user_1_and_2():
+
+    # RESET
+    req = urllib.request.Request(
+        f'{BASE_URL}/workspace/reset',
+        headers={'Content-Type': 'application/json'},
+        method='POST'
+    )
+
+    load(urllib.request.urlopen(req))
+
+    # REGISTER user_1
+    register_info_1 = dumps({
+        'email': 'z1234567@unsw.edu.au',
+        'password': 'thisisaPassword',
+        'name_first': 'Peter',
+        'name_last': 'Parker'
+    }).encode('utf-8')
+
+
+    req = urllib.request.Request(
+        f'{BASE_URL}/auth/register',
+        data=register_info_1,
+        headers={'Content-Type': 'application/json'},
+        method='POST'
+    )
+
+    load(urllib.request.urlopen(req))
+    
+    # REGISTER user_2
+    register_info_2 = dumps({
+        'email': 'z7654321@unsw.edu.au',
+        'password': 'thisisaPassword',
+        'name_first': 'Pete',
+        'name_last': 'Peteer'
+    }).encode('utf-8')
+
+
+    req = urllib.request.Request(
+        f'{BASE_URL}/auth/register',
+        data=register_info_2,
+        headers={'Content-Type': 'application/json'},
+        method='POST'
+    )
+
+    load(urllib.request.urlopen(req))
+    
+    # Login user_1
+    login_info = dumps({
+        'email': 'z1234567@unsw.edu.au',
+        'password': 'thisisaPassword'
+    }).encode('utf-8')
+
+    req = urllib.request.Request(
+        f'{BASE_URL}/auth/login',
+        data=login_info,
+        headers={'Content-Type': 'application/json'},
+        method='POST'
+    )
+
+    load(urllib.request.urlopen(req))
+
+    # Login user_2
+    login_info = dumps({
+        'email': 'z7654321@unsw.edu.au',
+        'password': 'thisisaPassword'
+    }).encode('utf-8')
+
+    req = urllib.request.Request(
+        f'{BASE_URL}/auth/login',
+        data=login_info,
+        headers={'Content-Type': 'application/json'},
+        method='POST'
+    )
+
+    load(urllib.request.urlopen(req))
+    #return payload
+
+def test_workspace_reset(register_and_login_user_1_and_2):
+    user_1_token = 'b\'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1X2lkIjoiMSJ9.N0asY15U0QBAYTAzxGAvdkuWG6CyqzsR_rvNQtWBmLg\''
+    user_2_token = 'b\'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1X2lkIjoiMiJ9.UNGv0HfSeyM4FtXkAc4HfuOl_HyNLFmRMeLx_4c0Ryg\''
+    
+    # user_1 creates a public channel
+    channel_info = dumps({
+        'token': user_1_token,
+        'name': 'a channel',
+        'is_public': True
+    }).encode('utf-8')
+
+    req = urllib.request.Request(
+        f'{BASE_URL}/channels/create',
+        data=channel_info,
+        headers={'Content-Type': 'application/json'},
+        method='POST'
+    )
+
+    load(urllib.request.urlopen(req))
+
+    # user_1 add user_2 as an owner
+    addowner_info = dumps({
+        'token': user_1_token,
+        'channel_id': 1,
+        'u_id': 2
+    }).encode('utf-8')
+
+    req = urllib.request.Request(
+        f'{BASE_URL}/channel/addowner',
+        data=addowner_info,
+        headers={'Content-Type': 'application/json'},
+        method='POST'
+    )
+
+    load(urllib.request.urlopen(req))
+
+    # RESET
+    req = urllib.request.Request(
+        f'{BASE_URL}/workspace/reset',
+        headers={'Content-Type': 'application/json'},
+        method='POST'
+    )
+
+    payload = load(urllib.request.urlopen(req))
+    assert payload == {}
+
+
+    # REGISTER user_1 AGAIN
+    register_info_1 = dumps({
+        'email': 'z1234567@unsw.edu.au',
+        'password': 'thisisaPassword',
+        'name_first': 'Peter',
+        'name_last': 'Parker'
+    }).encode('utf-8')
+
+
+    req = urllib.request.Request(
+        f'{BASE_URL}/auth/register',
+        data=register_info_1,
+        headers={'Content-Type': 'application/json'},
+        method='POST'
+    )
+
+    load(urllib.request.urlopen(req))
+
+    # Login user_1 AGAIN
+    login_info = dumps({
+        'email': 'z1234567@unsw.edu.au',
+        'password': 'thisisaPassword'
+    }).encode('utf-8')
+
+    req = urllib.request.Request(
+        f'{BASE_URL}/auth/login',
+        data=login_info,
+        headers={'Content-Type': 'application/json'},
+        method='POST'
+    )
+
+    load(urllib.request.urlopen(req))
+
+    #channels/listall
+
+    listall_infos = dumps({
+        'token': user_1_token
+    }).encode('utf-8')
+
+    req = urllib.request.Request(
+        f'{BASE_URL}/channels/listall',
+        data=listall_infos,
+        headers={'Content-Type': 'application/json'},
+        method='GET'
+    )
+
+    payload = load(urllib.request.urlopen(req))
+    assert payload == {'channels':[]}
+
+    # #users/all
+    userall_info = dumps({
+        'token': user_1_token
+    }).encode('utf-8')
+
+    req = urllib.request.Request(
+        f'{BASE_URL}/users/all',
+        data=userall_info,
+        headers={'Content-Type': 'application/json'},
+        method='GET'
+    )
+
+    payload = load(urllib.request.urlopen(req))
+    assert payload['users'][0]['u_id'] == 1
+
+
+
