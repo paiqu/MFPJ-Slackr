ed5b242e0bf97680e2644b34d61b3c8a38e2a61e
Xinlei Zhang <z5237609@ad.unsw.edu.au>
2020-04-09 10:06:16 +1000

    finished pylint

--- src/message_pin.py
+++ src/message_pin.py
@@ -3,8 +3,7 @@ from json import dumps
 from flask import Blueprint, request
 from check_functions import token_to_uid, channel_member_check, message_id_check
 from error import InputError, AccessError
-from data import DATA, getData
-from class_file import User, Channel, Message
+from data import getData
 
 PIN = Blueprint('message_pin', __name__)
 
@@ -19,51 +18,40 @@ def request_get():
 def message_pin(token, message_id):
     ''' Given a message within a channel, mark it as "pinned" to be given special display treatment by the frontend'''
     DATA = getData()
-    users = DATA['users']
     channels = DATA['channels']
     messages = DATA['messages']
-    '''
-    self-check function
-    users.append(vars(User(u_id=123, email='123@55.com', name_first='pai', name_last='qu', handle='')))   
-    channels.append(vars(Channel(1,"Public")))
-    channels[0]['members'].append(users[0])
-    channels[0]['owners'].append(users[0])
-    messages.append(vars(Message("hello",1,1,123,574820)))
-    '''
-    
+
     if not message_id_check(message_id):
         raise InputError("message_id is not a valid message")
-    
+
     # find the target_message
     for message in messages:
         if message['message_id'] == message_id:
             target_message = message
-            
+
     if not channel_member_check(target_message['channel_id'], token):
         raise AccessError("Authorised user is not a member of channel with channel_id")
-    
+
     authorised_user_id = token_to_uid(token)
     channel_id = target_message['channel_id']
     is_owner = False
-    
+
     # find the channel which the message in
     for channel in channels:
         if channel['channel_id'] == channel_id:
             target_channel = channel
-    
-    # check whether the token given is owner 
+
+    # check whether the token given is owner
     for owner in target_channel['owners']:
         if owner['u_id'] == authorised_user_id:
             is_owner = True
 
     if is_owner == False:
         raise InputError("The authorised user is not an owner")
-    
+
     if target_message['is_pin'] == True:
         raise InputError("Message with ID message_id is already pinned")
-    
-    target_message['is_pin'] = True
-    
-    return {}
- 
 
+    target_message['is_pin'] = True
+
+    return {}
